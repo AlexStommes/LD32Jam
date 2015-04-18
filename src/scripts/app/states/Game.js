0b795930f28e1ef0c3c25750a2b480d21ae83f71
Alex <alexstommes@gmail.com>
2015-04-18 12:55:36 -0500

    Encapsulate gradient creation.

--- src/scripts/app/states/Game.js
+++ src/scripts/app/states/Game.js
@@ -11,35 +11,12 @@ import Player from '../objects/Player';
 export default class Game extends Phaser.State {
 
   create () {
-    //robin's gradient:
     this.game.stage.backgroundColor = "fff";
-
-    var out = [];
-    var bmd = this.game.add.bitmapData(900, 600);
-    bmd.addToWorld();
-
-    // start of horizontal gradient
-    var myY = 100;
-
-    for (var i = 0; i < 600; i++)
-    {
-      var c = Phaser.Color.interpolateColor(0x2A8BCA, 0x38589C, 30, i);
-
-      // console.log(Phaser.Color.getWebRGB(c));
-
-      bmd.rect(0, myY, 900, myY+1, Phaser.Color.getWebRGB(c));
-
-      out.push(Phaser.Color.getWebRGB(c));
-
-      myY += 10;
-    }
-    // end robin's gradient
-
+    this.makeGradient(this.game);
+    
     //start of waves
     
     //end of waves
-
-
 
     let { centerX: x, centerY: y } = this.world;
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -62,6 +39,29 @@ export default class Game extends Phaser.State {
 
   makePlayer (x, y, speed = 100) {
     return new Player(this.game, x, y, speed);
+  }
+
+  makeGradient(game){
+    //robin's gradient:
+    var out = [];
+    var bmd = game.add.bitmapData(900, 600);
+    bmd.addToWorld();
+    // start of horizontal gradient
+    var myY = 100;
+
+    for (var i = 0; i < 600; i++)
+    {
+      var c = Phaser.Color.interpolateColor(0x2A8BCA, 0x38589C, 30, i);
+
+      // console.log(Phaser.Color.getWebRGB(c));
+
+      bmd.rect(0, myY, 900, myY+1, Phaser.Color.getWebRGB(c));
+
+      out.push(Phaser.Color.getWebRGB(c));
+
+      myY += 10;
+    }
+    // end robin's gradient
   }
 
 }

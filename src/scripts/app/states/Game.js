362b7e44cbe3c180b730fffc9ee953d5d723b0aa
Robin Nelson <robinanelson@gmail.com>
2015-04-18 10:36:43 -0700

    gradient added

--- src/scripts/app/states/Game.js
+++ src/scripts/app/states/Game.js
@@ -5,16 +5,47 @@
  * A sample Game state, displaying the Phaser logo.
  */
 
+
 import Player from '../objects/Player';
 
 export default class Game extends Phaser.State {
 
   create () {
+    //robin's gradient:
+    this.game.stage.backgroundColor = "fff";
+
+    var out = [];
+    var bmd = this.game.add.bitmapData(900, 600);
+    bmd.addToWorld();
+
+    // start of horizontal gradient
+    var myY = 100;
+
+    for (var i = 0; i < 600; i++)
+    {
+      var c = Phaser.Color.interpolateColor(0x2A8BCA, 0x38589C, 30, i);
+
+      // console.log(Phaser.Color.getWebRGB(c));
+
+      bmd.rect(0, myY, 900, myY+1, Phaser.Color.getWebRGB(c));
+
+      out.push(Phaser.Color.getWebRGB(c));
+
+      myY += 10;
+    }
+    // end robin's gradient
+
+    //start of waves
+    
+    //end of waves
+
+
+
     let { centerX: x, centerY: y } = this.world;
 
     this.logo = this.add.image(x, y, 'phaser');
     this.logo.anchor.set(0.5);
-	
+
     this.player = this.add.existing(this.makePlayer(x, y));
 
 	//this.player = this.add.image(x, y, 'player');

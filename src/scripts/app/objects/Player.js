a50164fb0fe4a69c7bf288ad3cedc9932254e5bc
Alex <alexstommes@gmail.com>
2015-04-18 19:12:59 -0500

    Cleanup Player.js.

--- src/scripts/app/objects/Player.js
+++ src/scripts/app/objects/Player.js
@@ -1,30 +1,7 @@
-/*
- * Player
- * ============================================================================
- *
- *
- * This is an example prefab, demonstrating how you could extend a Phaser
- * class to make your own game objects. You can extend from other Phaser
- * object classes too, not just Sprites. Refer to the `ActionsMenu` example
- * for a more advanced prefab.
- *
- * Here, I'm using a tween, instead of rotating the logo by updating its
- * angle, but you can do whatever you creativity demand you, so try that!
- */
-
-
 class Player extends Phaser.Sprite {
 
-
     constructor(game, x, y, speed = 10) {
-        // Remember to always use `super()` calls, whatever the class you're
-        // extending from, using the same constructor arguments you'd specify for
-        // regular Phaser classes.
         super(game, x, y, 'player', 'Player01');
-
-        // If you need to call `super` from overridden methods use the form
-        // `super.method(...)` call, passing the arguments required by that
-        // overridden method.
 
         this.speed = speed;
         this.anchor.set(0.5);
@@ -90,4 +67,4 @@ class Player extends Phaser.Sprite {
 }
 
 
-exportdefault Player;
+export default Player;
